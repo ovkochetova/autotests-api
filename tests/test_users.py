bf0d7570319b8ddcb2156e97f5da4b1f3c56a0c2
ovkochetova <ovkochetova@gmail.com>
2025-10-03 17:30:09 +0300

    Параметризация Pytest. Практика работы с pytest параметризацией

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,16 +3,17 @@ from clients.users.public_users_client import get_public_users_client, PublicUse
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
 from http import HTTPStatus
 import pytest
-
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
-from tools.assertions.users import assert_create_user_response, assert_user, assert_get_user_response
+from tools.assertions.users import assert_create_user_response, assert_get_user_response
+from tools.fakers import  fake
 
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user(public_users_client: PublicUsersClient):
-    create_request = CreateUserRequestSchema()
+@pytest.mark.parametrize("domain", ["mail.ru", "gmail.com", "example.com"])
+def test_create_user(domain: str,  public_users_client: PublicUsersClient):
+    create_request = CreateUserRequestSchema(email=fake.email(domain=domain))
     create_response = public_users_client.create_user_api(create_request)
     create_response_data = CreateUserResponseSchema.model_validate_json(create_response.text)
 
--- tools/fakers.py
+++ tools/fakers.py
@@ -1,5 +1,47 @@
-import time
+import pytest
+from faker import Faker
 
-def get_random_email() -> str:
-    return f"test.{time.time()}@example.com"
-print(time.time())
+class Fake:
+    def __init__(self, faker: Faker):
+        self.faker = faker
+
+    def  text(self)-> str:
+        return self.faker.text()
+
+    def uuid4(self)-> str:
+        return self.faker.uuid4()
+
+    def email(self,  domain: str | None = None)-> str:
+        """
+        Генерирует случайный email.
+
+        :param domain: Домен электронной почты (например, "example.com").
+        Если не указан, будет использован случайный домен.
+        :return: Случайный email.
+        """
+        return self.faker.email(domain=domain)
+
+    def sentence(self)-> str:
+        return self.faker.sentence()
+
+    def password(self)-> str:
+        return self.faker.password()
+    def last_name(self)-> str:
+        return self.faker.last_name()
+    def first_name(self)-> str:
+        return self.faker.first_name()
+    def middle_name(self)-> str:
+        return self.faker.first_name()
+
+    def estimated_time(self)-> str:
+        return f"{self.integer(1, 10)} weeks"
+
+    def integer(self, start: int=1, end: int=100)-> int:
+        return self.faker.random_int(start, end)
+
+    def max_score(self)-> int:
+        return self.integer(50 , 100)
+    def min_score(self)-> int:
+        return self.integer(1, 49)
+
+fake = Fake(faker=Faker("ru_RU"))

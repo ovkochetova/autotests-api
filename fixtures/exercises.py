d5db698e28e3b49392431eb905cb821f0ba7323f
ovkochetova <ovkochetova@gmail.com>
2025-10-06 19:23:09 +0300

    Рефакторинг и подготовка к написанию API автотестов. Практика реализации фикстур

--- fixtures/exercises.py
+++ fixtures/exercises.py
@@ -10,12 +10,12 @@ class ExerciseFixture(BaseModel):
     request: CreateExerciseSchema
     response: CreateExerciseResponseSchema
 
-@pytest.fixture(scope = "function")
+@pytest.fixture
 def exercises_client(function_user: UserFixture) -> ExercisesClient:
     return get_exercises_client(function_user.authentication_user)
 
-@pytest.fixture(scope = "function")
-def function_exercise(function_user: UserFixture, function_course: CourseFixture) -> ExerciseFixture:
-    request: CreateExerciseSchema
+@pytest.fixture
+def function_exercise(exercises_client: ExercisesClient, function_user: UserFixture, function_course: CourseFixture) -> ExerciseFixture:
+    request: CreateExerciseSchema(courseId=function_course.response.course.id)
     response = exercises_client.create_exercise(request)
     return ExerciseFixture(request=request,response=response)

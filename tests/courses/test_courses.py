9ed2195323b6cb650b1fcc1fac3e2db4f4220533
ovkochetova <ovkochetova@gmail.com>
2025-10-09 18:12:58 +0300

    Практикуемся в написании API автотестов. Часть 2. Практика написания API автотестов для работы с курсами

---
+++ tests/courses/test_courses.py
@@ -0,0 +1,75 @@
+from http import HTTPStatus
+
+import pytest
+
+from clients.courses.courses_client import CoursesClient
+from clients.courses.courses_schema import UpdateCourseRequestSchema, UpdateCourseResponseSchema, GetCoursesQuerySchema, \
+    GetCoursesResponseSchema, CreateCourseRequestSchema, CreateCourseResponseSchema
+from fixtures.courses import CourseFixture
+from fixtures.files import FileFixture
+from fixtures.users import UserFixture
+from tools.assertions.base import assert_status_code
+from tools.assertions.course import assert_update_course_response, assert_get_courses_response, \
+    assert_create_course_response
+from tools.assertions.schema import validate_json_schema
+
+
+@pytest.mark.courses
+@pytest.mark.regression
+class TestCourses:
+    def test_create_course(
+            self,
+            courses_client: CoursesClient,
+            function_course: CourseFixture,
+            function_user: UserFixture,
+            function_file: FileFixture
+    ):
+        # Формируем параметры запроса, передавая preview_file_id  и created_by_user_id
+        request = CreateCourseRequestSchema(preview_file_id = function_file.response.file.id, created_by_user_id = function_user.response.user.id)
+        # Отправляем POST-запрос на создание курса
+        response = courses_client.create_course_api(request)
+        # Десериализуем JSON-ответ в Pydantic-модель
+        response_data = CreateCourseResponseSchema.model_validate_json(response.text)
+
+        # Проверяем, что код ответа 200 OK (Но скорее всего надо было 201;) )
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        # Проверяем, что список данные, переданные при создании совпадают с данными, возвращенными от сервера
+        assert_create_course_response(request, response_data)
+
+        # Проверяем соответствие JSON-ответа схеме
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_get_courses(
+            self,
+            courses_client: CoursesClient,
+            function_user: UserFixture,
+            function_course: CourseFixture
+    ):
+        # Формируем параметры запроса, передавая user_id
+        query = GetCoursesQuerySchema(user_id = function_user.response.user.id)
+        print('query:', query)
+        # Отправляем GET-запрос на получение списка курсов
+        response = courses_client.get_courses_api(query)
+        # Десериализуем JSON-ответ в Pydantic-модель
+        response_data = GetCoursesResponseSchema.model_validate_json(response.text)
+        print('response_data:', response_data)
+
+        # Проверяем, что код ответа 200 OK
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        # Проверяем, что список курсов соответствует ранее созданным курсам
+        assert_get_courses_response(response_data, [function_course.response])
+
+        # Проверяем соответствие JSON-ответа схеме
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_update_course(self, courses_client: CoursesClient, function_course: CourseFixture):
+        request = UpdateCourseRequestSchema()
+        response = courses_client.update_course_api(function_course.response.course.id, request)
+        response_data = UpdateCourseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_update_course_response(request, response_data)
+
+        validate_json_schema(response.json(), response_data.model_json_schema())
+        print('response.json():', response.json())
+        print('response_date.model_json_schema():', response_data.model_json_schema())

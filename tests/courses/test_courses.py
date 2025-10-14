1ebffd638124873d3bd404c7cb2103d53e1fa48c
ovkochetova <ovkochetova@gmail.com>
2025-10-14 11:46:41 +0300

    Интеграция Allure в автотесты. Практическая работа с Allure аннотациями

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -8,15 +8,29 @@ from clients.courses.courses_schema import UpdateCourseRequestSchema, UpdateCour
 from fixtures.courses import CourseFixture
 from fixtures.files import FileFixture
 from fixtures.users import UserFixture
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
 from tools.assertions.base import assert_status_code
 from tools.assertions.course import assert_update_course_response, assert_get_courses_response, \
     assert_create_course_response
 from tools.assertions.schema import validate_json_schema
-
+import allure
+from allure_commons.types import Severity
 
 @pytest.mark.courses
 @pytest.mark.regression
+@allure.tag(AllureTag.COURSES, AllureTag.REGRESSIONS)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.COURSES)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.COURSES)
 class TestCourses:
+    @allure.story(AllureStory.CREATE_ENTITY)
+    @allure.title("Create courses")
+    @allure.severity(Severity.BLOCKER)
+    @allure.sub_suite(AllureStory.CREATE_ENTITY)
     def test_create_course(
             self,
             courses_client: CoursesClient,
@@ -38,6 +52,10 @@ class TestCourses:
         # Проверяем соответствие JSON-ответа схеме
         validate_json_schema(response.json(), response_data.model_json_schema())
 
+    @allure.story(AllureStory.GET_ENTITY)
+    @allure.title("Get course")
+    @allure.severity(Severity.BLOCKER)
+    @allure.sub_suite(AllureStory.GET_ENTITY)
     def test_get_courses(
             self,
             courses_client: CoursesClient,
@@ -61,6 +79,10 @@ class TestCourses:
         # Проверяем соответствие JSON-ответа схеме
         validate_json_schema(response.json(), response_data.model_json_schema())
 
+    @allure.story(AllureStory.UPDATE_ENTITY)
+    @allure.title("Update course")
+    @allure.severity(Severity.CRITICAL)
+    @allure.sub_suite(AllureStory.UPDATE_ENTITY)
     def test_update_course(self, courses_client: CoursesClient, function_course: CourseFixture):
         request = UpdateCourseRequestSchema()
         response = courses_client.update_course_api(function_course.response.course.id, request)
--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 
+import allure
 import pytest
 from httpx import request
 
@@ -10,15 +11,27 @@ from clients.exercises.exercises_schema import CreateExerciseSchema, CreateExerc
     GetExercisesResponseSchema
 from fixtures.courses import CourseFixture
 from fixtures.exercises import ExerciseFixture
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
 from tools.assertions.base import assert_status_code
 from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response, \
     assert_update_exercise_response, assert_exercise_not_found_response, assert_get_exercises_response
 from tools.assertions.schema import validate_json_schema
+from allure_commons.types import Severity
 
 
 @pytest.mark.exercises
 @pytest.mark.regression
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.EXERCISES)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.EXERCISES)
 class TestExercises:
+    @allure.story(AllureStory.CREATE_ENTITY)
+    @allure.title("Create Exercise")
+    @allure.severity(Severity.BLOCKER)
+    @allure.sub_suite(AllureStory.CREATE_ENTITY)
     def test_create_exercise(self, exercises_client: ExercisesClient, function_course: CourseFixture):
         # Формируем параметры запроса, передавая course_id
         request = CreateExerciseSchema(course_id = function_course.response.course.id)
@@ -36,6 +49,11 @@ class TestExercises:
         validate_json_schema(response.json(), response_data.model_json_schema())
 
         # Отправляем GET-запрос на запрос курса по его ид
+
+    @allure.story(AllureStory.GET_ENTITY)
+    @allure.title("Get Exercise")
+    @allure.severity(Severity.BLOCKER)
+    @allure.sub_suite(AllureStory.GET_ENTITY)
     def test_get_exercise(self, exercises_client: ExercisesClient, function_exercise: ExerciseFixture):
         response = exercises_client.get_exercise_api(function_exercise.response.exercise.id)
         # Десериализуем JSON-ответ в Pydantic-модель
@@ -49,6 +67,10 @@ class TestExercises:
         # Проверяем соответствие JSON-ответа схеме
         validate_json_schema(response.json(), response_data.model_json_schema())
 
+    @allure.story(AllureStory.UPDATE_ENTITY)
+    @allure.title("Update Exercise")
+    @allure.severity(Severity.CRITICAL)
+    @allure.sub_suite(AllureStory.UPDATE_ENTITY)
     def test_update_exercise(self, exercises_client: ExercisesClient, function_exercise: ExerciseFixture):
         # Формируем модель запроса
         request = UpdateExerciseRequestSchema()
@@ -65,6 +87,10 @@ class TestExercises:
         # Проверяем соответствие JSON-ответа схеме
         validate_json_schema(response.json(), response_data.model_json_schema())
 
+    @allure.story(AllureStory.DELETE_ENTITY)
+    @allure.title("Delete Exercise")
+    @allure.severity(Severity.CRITICAL)
+    @allure.sub_suite(AllureStory.DELETE_ENTITY)
     def test_delete_exercise(self, exercises_client: ExercisesClient, function_exercise: ExerciseFixture):
         # Удаляем задание
         delete_response = exercises_client.delete_exercise_api(exercise_id=function_exercise.response.exercise.id)
@@ -82,6 +108,10 @@ class TestExercises:
         # Проверяем, что ответ соответствует схеме
         validate_json_schema(get_response.json(), get_response_data.model_json_schema())
 
+    @allure.story(AllureStory.GET_ENTITIES)
+    @allure.title("Get Exercises")
+    @allure.severity(Severity.BLOCKER)
+    @allure.sub_suite(AllureStory.GET_ENTITIES)
     def test_get_exercises(self, exercises_client: ExercisesClient, function_exercise: ExerciseFixture, function_course: CourseFixture):
         # Формируем структуру запроса
         query = GetExerciseQuerySchema(course_id=function_course.response.course.id)

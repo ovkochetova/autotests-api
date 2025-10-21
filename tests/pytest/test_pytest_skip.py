6aad2dbb23dd98b51cdb855c772b165442e3468e
ovkochetova <ovkochetova@users.noreply.github.com>
2025-10-21 15:29:11 +0000

    deploy: f2138593f8b2d0cf885030dd5652de031bff0120

--- tests/pytest/test_pytest_skip.py
+++
@@ -1,5 +0,0 @@
-# import pytest
-#
-# @pytest.mark.skip(reason="фича в разработке")
-# def test_feature_in_development():
-#     ...
--- tools/allure/epics.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class AllureEpic(str, Enum):
-    LMS = "LMS service"
-    STUDENT = "STUDENT service"
-    ADMIN = "ADMIN service"
